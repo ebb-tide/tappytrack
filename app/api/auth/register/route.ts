01b67bbfd42b28886c3f9855e53926c62dd57a5a
ebb-tide <denizk@gmail.com>
2025-06-26 20:38:53 -0700

    remove api route

--- app/api/auth/register/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(){//req: NextRequest) {
-  // const userData = await req.json(); 
-  // console.dir(userData);
-
-  return NextResponse.json({ success: true });
-}
